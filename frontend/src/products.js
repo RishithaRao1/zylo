119dda7250bb143085311e78549a0dde712bf9e3
RishithaRao1 <rishithamadhavaram765@gmail.com>
2024-06-29 04:37:25 +0530

    ratings and numReviews

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -7,6 +7,8 @@ const products = [
       "These tailored shorts are crafted from high-quality fabric for a comfortable fit. Ideal for both casual and semi-formal occasions, they feature a classic design with a single button closure and belt loops",
     category: 'Mens/Tops',
     price: 35.00,
+    rating: 4,
+    numReviews: 120,
   },
   {
     _id: '2',
@@ -16,6 +18,8 @@ const products = [
       "A classic men's polo shirt made from breathable cotton, perfect for everyday wear. This shirt features a three-button placket and a comfortable fit, making it a versatile addition to any wardrobe",
     category: 'Mens/Tops',
     price: 25.00,
+    rating: 3,
+    numReviews: 85,
   },
   {
     _id: '3',
@@ -25,6 +29,8 @@ const products = [
       "Comfortable and stylish, these drawstring shorts are perfect for casual outings. Made from lightweight fabric, they offer a relaxed fit and feature an adjustable waistband for added comfort",
     category: 'Mens/Bottoms',
     price: 30.00,
+    rating: 5,
+    numReviews: 200,
   },
   {
     _id: '4',
@@ -34,6 +40,8 @@ const products = [
       "A staple for any wardrobe, this men's crew neck t-shirt is made from soft, high-quality cotton. It offers a classic fit and is available in multiple colors to suit any style",
     category: 'Mens/Tops',
     price: 20.00,
+    rating: 4,
+    numReviews: 150,
   },
   {
     _id: '5',
@@ -43,6 +51,8 @@ const products = [
       "This versatile button-up shirt is perfect for both casual and formal settings. Made from premium cotton, it features a chest pocket and buttoned cuffs for a polished look",
     category: 'Mens/Tops',
     price: 40.00,
+    rating: 5,
+    numReviews: 180,
   },
   {
     _id: '6',
@@ -52,6 +62,8 @@ const products = [
       "A stylish beige button-up shirt suitable for various occasions. Crafted from soft, durable fabric, it offers a comfortable fit and features a classic collar and front pocket.",
     category: 'Mens/Tops',
     price: 40.00,
+    rating: 4,
+    numReviews: 130,
   },
   {
     _id: '7',
@@ -61,6 +73,8 @@ const products = [
       "Designed for active lifestyles, this performance t-shirt is made from moisture-wicking fabric to keep you cool and dry. It offers a comfortable fit and is perfect for workouts or casual wear.",
     category: 'Mens/Tops',
     price: 25.00,
+    rating: 4,
+    numReviews: 100,
   },
   {
     _id: '8',
@@ -70,6 +84,8 @@ const products = [
       "These men's drawstring shorts feature a unique patch design and are made from comfortable fabric. They offer a relaxed fit and an adjustable waistband, perfect for casual wear",
     category: 'Mens/Bottoms',
     price: 28.00,
+    rating: 4,
+    numReviews: 110,
   },
   {
     _id: '9',
@@ -79,6 +95,8 @@ const products = [
       "These light wash jeans offer a modern look with a slim fit. Made from durable denim, they are perfect for both casual and semi-formal occasions.",
     category: 'Mens/Bottoms',
     price: 45.00,
+    rating: 4,
+    numReviews: 90,
   },
   {
     _id: '10',
@@ -88,6 +106,8 @@ const products = [
       "A pair of dark blue jeans with a classic fit. These jeans are versatile and can be paired with a variety of outfits for different occasions.",
     category: 'Mens/Bottoms',
     price: 45.00,
+    rating: 3,
+    numReviews: 70,
   },
   {
     _id: '11',
@@ -97,6 +117,8 @@ const products = [
       "These camel-colored chino pants are made from high-quality fabric, offering comfort and style. They are ideal for both casual and business casual settings.",
     category: 'Mens/Bottoms',
     price: 40.00,
+    rating: 4,
+    numReviews: 120,
   },
   {
     _id: '12',
@@ -106,6 +128,8 @@ const products = [
       "These blue jogger pants are perfect for a relaxed and sporty look. They feature an elastic waistband and cuffs for a comfortable fit.",
     category: 'Mens/Bottoms',
     price: 30.00,
+    rating: 5,
+    numReviews: 160,
   },
 ];
 
