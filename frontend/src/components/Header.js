b45354d80bd7b7050558da7464e3b14a67d71793
RishithaRao1 <rishithamadhavaram765@gmail.com>
2024-06-29 06:20:02 +0530

    productscreen:product Details

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>Zylo</Navbar.Brand>
+            <Navbar.Brand style={{ color: '#fc1d1' }}>Zylo</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,15 +1,16 @@
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Rating from './Rating';
+import '../assets/index.css'
 
 const Product = ({ product }) => {
   return (
-    <Card className='my-3 p-3 rounded'>
+    <Card className='my-3 p-3 rounded product-card'>
       <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top' />
       </Link>
 
-      <Card.Body>
+      <Card.Body className='card-body'>
         <Link to={`/product/${product._id}`}>
           <Card.Title as='div' className='product-title'>
             <strong>{product.name}</strong>
@@ -19,12 +20,11 @@ const Product = ({ product }) => {
         <Card.Text as='div'>
           <Rating
             value={product.rating}
-            
             text={`${product.numReviews} Reviews`}
           />
         </Card.Text>
 
-        <Card.Text as='h3'>${product.price}</Card.Text>
+        <Card.Text as='h3' className='product-price'>${product.price}</Card.Text>
       </Card.Body>
     </Card>
   );
